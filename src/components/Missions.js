6c96043a78a413d651bcadbcfc41d5471fa35c84
Larissa Cristina <larissacristina.nunes@outlook.com>
2022-01-25 19:25:58 -0300

    Requisito 10 - Missoes na tela

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {
+          missions.map((missão) => {
+            const { name, year, country, destination } = missão;
+            return (
+              <MissionCard
+                key={ name }
+                name={ name }
+                year={ year }
+                country={ country }
+                destination={ destination }
+              />);
+          })
+        }
       </div>
     );
   }
